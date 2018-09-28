b90dc703558c5d8ac94108b56ad70f84afa45b2d
oliegjio <oliegjio@gmail.com>
2018-09-28 17:35:27 +0000

    Refactoring plotting frequencies

--- src/plot_frequencies.py
+++ src/plot_frequencies.py
@@ -5,11 +5,11 @@ from scipy.io import wavfile
 def plot_frequencies(path):
 
     fs, data = wavfile.read(path)
-    a = data.T[0]
 
-    b=[(ele / 2 ** 8.) * 2 - 1 for ele in a]
+    a = data.T[0]
+    b = [(e / 2 ** 8.) * 2 - 1 for e in a]
     c = fft(b)
-    d = int(len(c)/2)
+    d = int(len(c) / 2)
 
     plt.plot(abs(c[:(d - 1)]), 'r') 
     plt.show()
