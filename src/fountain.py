55a11db28b1aeb63c2282eb7b8d95010778f1269
oliegjio <oliegjio@gmail.com>
2018-10-01 12:31:51 +0300

    Improved make_command method from Fountain class

--- src/fountain.py
+++ src/fountain.py
@@ -17,9 +17,13 @@ class Fountain:
     part_delimiter = ':' # Between circuit and action.
     command_delimiter = '|' # Between different commands.
     circuit_label = 'm'
+    pipe_label = 'x'
 
+    # Method to construct fountain commands.
+    #
     # `time` - Command start time.
-    # `args` - Every even element is a circuit number. Every odd element is an action.
+    # `args` - Every even element is either a pair of circuit number and pipe number or
+    #          circuit number only. Every odd element is an action.
     #          You can pass multiple circuit-action pairs to create a complex command.
     def make_command(self, time, *args):
         if (len(args) % 2 != 0 or len(args) < 2):
@@ -29,7 +33,11 @@ class Fountain:
         command = str(time) + self.time_delimiter
 
         for n in map(lambda x: x * 2, range(len(args) / 2)):
-            command += self.circuit_label + str(args[n]) + self.part_delimiter + str(args[n + 1])
+            if isinstance(args[0], tuple):
+                circuit, pipe = args[n]
+                command += self.circuit_label + str(circuit) + self.pipe_label + str(pipe) + self.part_delimiter + str(args[n + 1])
+            else:
+                command += self.circuit_label + str(args[n]) + self.part_delimiter + str(args[n + 1])
             if n != len(args) - 2:
                 command += self.command_delimiter
 
--- src/test-beat-tracking.py
+++ src/test-beat-tracking.py
@@ -8,26 +8,27 @@ from fountain import *
 np.seterr(invalid='ignore')
 
 def get_beat_times(path):
-  x, sr = librosa.load(path)
+    x, sr = librosa.load(path)
 
-  proc = madmom.features.beats.DBNBeatTrackingProcessor(fps=100)
-  act = madmom.features.beats.RNNBeatProcessor()(path)
+    proc = madmom.features.beats.DBNBeatTrackingProcessor(fps=100)
+    act = madmom.features.beats.RNNBeatProcessor()(path)
 
-  beat_times = proc(act)
+    beat_times = proc(act)
 
-  return beat_times
+    return beat_times
 
 def beat_times_to_sequence(beat_times):
   sequence = []
   sequence.append('0.00\t')
 
 def main(argv):
-  # path = argv[0]
-  # print(get_beat_times(path))
+    # path = argv[0]
+    # print(get_beat_times(path))
 
-  fountain = Fountain()
-  print(fountain.turn_on_circuit(10, 5))
-  print(fountain.turn_off_circuit(11, 5))
-  print(fountain.make_command(7, 5, 'test', 6, 'test2'))
+    fountain = Fountain()
+    print(fountain.turn_on_circuit(10, 5))
+    print(fountain.turn_off_circuit(11, 5))
+    print(fountain.make_command(7, 5, 'test', 6, 'test2'))
+    print(fountain.make_command(7, (5, 3), 'aa', (2, 3), 'tasd'))
 
 if __name__ == '__main__': main(sys.argv[1:])
