abd073ea55a5cdd135e7ffa7a948ad62089baa65
oliegjio <oliegjio@gmail.com>
2018-10-05 15:05:29 +0300

    Added code comments for 'image.py'

--- src/image.py
+++ src/image.py
@@ -6,6 +6,8 @@ CROP_NAME = 'crop'
 CROP_EXT = 'png'
 
 def pixel_strips(image_path, output_directory):
+    """ Crop image on strips with width 1 and height of the image. """
+
     image = Image.open(image_path)
     image_width, image_height = image.size
     for i in range(image_width):
@@ -18,13 +20,19 @@ def count_files_in_directory(directory_path):
     return len(files)
 
 def crop_name(index):
+    """ Get crop name with index. """
     return CROP_NAME + str(index) + '.' + CROP_EXT
 
 def crops_size(crops_path):
+    """ Get crops size in the directory. """
     image = Image.open(os.path.join(crops_path, crop_name(0)))
     return image.size
 
 def crops_pixel_data(crops_path):
+    """ Returns 2D array with size: N * M, N - image width, M - image height.
+    Every element of outer array is N-th crop RGB data.
+    Every element of inner array is (R, G, B) tuple of N x M pixel.
+    """
     crops_number = count_files_in_directory(crops_path)
     crops_width, crops_height = crops_size(crops_path)
     data = []
@@ -44,6 +52,6 @@ def main(argv):
 
     crops_path = argv[0]
     data = crops_pixel_data(crops_path)
-    print(data[1])
+    print(data[1498])
 
 if __name__ == '__main__': main(sys.argv[1:])
