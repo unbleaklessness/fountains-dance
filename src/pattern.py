db62121b54c17c1583bf9e12e309cd35c4a8dd1b
Dupeljan Linux <dupeljan@gmail.com>
2018-10-06 19:27:53 +0300

    Add 4 paterns

--- src/pattern.py
+++ src/pattern.py
@@ -3,7 +3,10 @@ from fountain import Fountain, Color
 class Patterns(object):
 	"""docstring for Patterns"""
 	f = Fountain()
+	collors = ('r','g','b','y','m','c','w')
 	start_time = 0
+	duration = 10000
+	count_of_elem = 15
 
 	
 	def get_pattern(self):
@@ -13,10 +16,9 @@ class Patterns(object):
 	def full_leap(self):
 		commands =[]
 		var = []
-		delta_time = 1000
-		for i in range(12):
+		for i in range(self.count_of_elem):
 		    var.append( self.f.turn_on_pumps(self.start_time,i) )
-		    var.append( self.f.turn_off_pumps(self.start_time + delta_time,i) )
+		    var.append( self.f.turn_off_pumps(self.start_time + self.duration,i) )
 		commands.append( self.f.combine( *var[0::2]) )
 		commands.append( self.f.combine( *var[1::2]) )
 
@@ -32,13 +34,13 @@ class Patterns(object):
 		#body
 
 		for p in range(peroids):
-		    for i in range(12):
+		    for i in range(self.count_of_elem):
 		        var.append(self.f.turn_on_pumps(period_duration* p  + self.start_time,(1,i) ) )
 		        var.append(self.f.turn_on_pumps(period_duration* p  + self.start_time,(3,i) ) )
 		        
 		    commands.append( self.f.combine(*var) )
 		    var = []
-		    for i in range(12):
+		    for i in range(self.count_of_elem):
 		        var.append(self.f.turn_on_pumps( period_duration* p  + self.start_time + period_duration / 2, (2,i) ) )
 		        var.append(self.f.turn_off_pumps(period_duration* p  + self.start_time + period_duration / 2,(1,i) ) )
 		        var.append(self.f.turn_off_pumps(period_duration* p  + self.start_time + period_duration / 2,(3,i) ) )
@@ -46,13 +48,30 @@ class Patterns(object):
 		    
 		    commands.append( self.f.combine(*var) )        
 		    var = []
-		    for i in range(12): 
+		    for i in range(self.count_of_elem): 
 		        var.append(self.f.turn_off_pumps(period_duration* p  + self.start_time + period_duration , (2,i) ) )
 		    commands.append( self.f.combine(*var) )
 		    var = []   
 		#End
 		
 		return commands
+
+	def pattern1(self):
+		commands = []
+		colors = ("c","m","y")
+		delta_time = 1.5
+		commands.append( "%s\tl10:y|m10:sf(15)|m1x1:sf(30)|m1x2:sf(30)|l1x1:r|l1x2:r|m2x1:sf(30)|m2x2:sf(30)|l2x1:c|l2x2:c|m3x1:sf(30)|m3x2:sf(30)|l3x1:m|l3x2:m\n" % self.f.format_milliseconds(self.start_time) )
+		new_time = self.start_time + 0.5
+		for i in range(1,4):
+			commands.append("%.2f\tm%dx1:sf(100)|m%dx2:sf(100)|l1x1:%s|l1x2:%s|l2x1:%s|l2x2:%s|l3x1:%s" % (new_time,i,i,colors[(i-1) % 3],colors[(i-1) % 3],colors[i % 3],colors[i % 3],colors[(i+1) % 3]) )
+			if i!=1:
+				commands.append( "|m%dx1:sf(30)|m%dx2:sf(30)" % (i-1,i-1) )
+			commands.append("\n")
+			new_time += delta_time
+		commands.append("%0.2f\tm1:off|m2:off|m3:off|m10:off\n" % new_time )
+
+		return commands
+
 	def rest(self): 
 		duration = 10000
 		commands =[]
@@ -61,8 +80,16 @@ class Patterns(object):
 
 		return commands
 
+	def set_lamps(self,color):
+		commands =[]
+		var = []
+		for i in range(self.count_of_elem):
+			var.append("%s\tl%d:%s\n" % ( self.f.format_milliseconds(self.start_time  ), i, color ))
+		commands.append( self.f.combine( *var ))
+		return commands
+
 p = Patterns()
-commands = p.rest() 
+commands = p.rest() 	
 out = file("partiture.txt","w")
 for i in range(len(commands) ):
     out.write(commands[i])
